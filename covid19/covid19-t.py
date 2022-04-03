adb71791aa4e6fa3c5dd14784653ec314c464547
Ito-Naoto <80243646+ITO-Naoto@users.noreply.github.com>
2022-04-03 17:19:05 +0900

    csv完成

--- covid19/covid19-t.py
+++ covid19/covid19-t.py
@@ -1,6 +1,7 @@
 # パッケージ読み込み
 import datetime as mdates
 import datetime
+from nturl2path import pathname2url
 import matplotlib as plt
 import pandas as pd
 import numpy as np
@@ -8,8 +9,9 @@ import csv
 import os
 
 # csv作成
-file = os.path.dirname(__file__)
-print(file)
+path1 = os.path.dirname(__file__)
+path2 = "covid19-tottori.csv"
+path = os.path.join(path1, path2)
 url = 'https://www.pref.tottori.lg.jp/item/1248786.htm#itemid1248786'
 data = pd.read_html(url, header=1)
 data1 = data[0].head(1)
@@ -22,12 +24,9 @@ dt = datetime.datetime.today()
 t_now = dt.date().strftime('%Y/%m/%d')
 add = np.array([[t_now,tottori,kurayosi,yonago,sum]]).reshape(1, 5)
 df_add = pd.DataFrame(add, columns=['Date','鳥取市保健所','倉吉保健所','米子保健所','Sum'])
-#csvを生成
-body = [
-        [t_now,tottori,kurayosi,yonago,sum], 
-       ]
-with open(os.path.dirname(__file__)/covid-19-tottori.csv, 'a',encoding='utf-8_sig',newline="") as f:
-    writer = csv.writer(f, lineterminator='\n')
+#csvに書き込み
+with open(path, 'a',encoding='utf-8_sig',newline="") as f:
+    writer = csv.writer(f, lineterminator = '\r')
     for ary in df_add.values:
         writer.writerow(ary)
 f.close()
