81cb72d9a8ecd0d8252d63ee82ea2887e7fb74a7
ITO Naoto <80243646+ITO-Naoto@users.noreply.github.com>
2022-04-05 14:51:40 +0900

    コードを単純化した

--- covid19/script/covid19-t-graph.py
+++ covid19/script/covid19-t-graph.py
@@ -13,29 +13,47 @@ import japanize_matplotlib
 path0 = os.path.dirname(__file__)
 path1 = os.path.dirname(path0)
 path2 = os.path.join('Data', 'csv', "covid19-tottori.csv")
-csvpath  = os.path.join(path1, path2)
+csvpath = os.path.join(path1, path2)
 
 # csv読み込み
-df = pd.read_csv(csvpath,parse_dates=['Date'],header=0)
+df = pd.read_csv(csvpath, parse_dates=['Date'], header=0)
+# グラフ共通設定
+t = df['Date'].values
+y = df['Sum'].values
+DAY = pd.to_timedelta(1, 'day')
+t1 = pd.to_datetime('2022-02-01')
+t2 = t[-1] + DAY
+# グラフをいい感じにする。
+locator = mdates.AutoDateLocator()
+formatter = mdates.ConciseDateFormatter(locator)
+# 現在時刻を取得，dt_nowに格納
+dt_now = datetime.datetime.now()
+dt_now_str = dt_now.strftime('%Y/%m/%d %H:%M:%S')
 
 # グラフ作成：1/2
+
+# 上の設定を代入
+fig, ax = plt.subplots()
+ax.xaxis.set_major_locator(locator)
+ax.xaxis.set_major_formatter(formatter)
 df2 = df.fillna(0)
-t = df2['Date']
 data0 = df2['Sum']
 data1 = df2['東部']
-data2 = df2['中部']  
+data2 = df2['中部']
 data3 = df2['西部']
-plt.bar(t,data0,label='総計',width=1.0,align="center",edgecolor='black')
-plt.bar(t, data1, label="東部",width=1.0,align='center',edgecolor='black')
-plt.bar(t, data2, bottom=data1, label="中部",width=1.0,align='center',edgecolor='black')
-plt.bar(t, data3, bottom=data1 + data2, label="西部",width=1.0,align='center',edgecolor='black')
-plt.title("鳥取県コロナ 地区ごとの推移", fontsize=15)
-plt.xlabel("")
-plt.ylabel("感染者数")
-plt.legend()
+ax.bar(t, data0, label='総計', width=DAY, align="edge", edgecolor='black')
+ax.bar(t, data1, label="東部", width=DAY, align='edge', edgecolor='black')
+ax.bar(t, data2, bottom=data1, label="中部", width=DAY, align='edge', edgecolor='black')
+ax.bar(t, data3, bottom=data1 + data2, label="西部", width=DAY, align='edge', edgecolor='black')
+ax.set_xlim(t1, t2)
+ax.set_title("地区ごと感染者数の推移", fontsize=15, loc='left', pad=30)
+fig.text(0.9, 0.89, '更新日時: ' + dt_now_str, horizontalalignment='right')
+# ax.xlabel("")
+# ax.ylabel("感染者数")
+ax.legend()
 path3 = os.path.join('Data', 'fig', 'graph', "tottori-area.svg")
-figpath1  = os.path.join(path1, path3)
-plt.savefig(figpath1, bbox_inches="tight")
+figpath1 = os.path.join(path1, path3)
+fig.savefig(figpath1, bbox_inches="tight")
 
 # グラフ作成:2/2
 maxt = df['Sum'].max()
@@ -43,49 +61,36 @@ maxt_line = df['Sum'].idxmax()
 maxt_date = df.iloc[maxt_line, 0].strftime('%Y/%m/%d')
 maxt_str = '最高感染者数:' + str(maxt)+'人'+'（{}）'
 maxt_date_print = maxt_str.format(maxt_date)
-# 現在時刻を取得，dt_nowに格納
-dt_now = datetime.datetime.now()
-dt_now_str = dt_now.strftime('%Y/%m/%d %H:%M:%S')
-# tへDateの値を格納
-t = df['Date'].values
-y = df['Sum'].values
 
-DAY = pd.to_timedelta(1, 'day')
-t1 = pd.to_datetime('2022-02-01')
-t2 = t[-1] + DAY
+
 # グラフを書く。
-fig,ax = plt.subplots()
-
+fig, ax = plt.subplots()
+ax.xaxis.set_major_locator(locator)
+ax.xaxis.set_major_formatter(formatter)
 cmap = plt.get_cmap('tab20c')
-#https://github.com/okumuralab/okumuralab.github.io/blob/master/python/code/COVID-tokyo.py の45,46行目を引用。一部変更
-#colにcmapをリスト
+# https://github.com/okumuralab/okumuralab.github.io/blob/master/python/code/COVID-tokyo.py の45,46行目を引用。一部変更
+# colにcmapをリスト
 col = [cmap(0), cmap(0), cmap(0), cmap(0), cmap(0), cmap(2), cmap(2)]
-#numpyの配列を作成
+# numpyの配列を作成
 cols = np.array([col[pd.Timestamp(i).dayofweek] for i in t])
-#ここまで
-#https://github.com/okumuralab/okumuralab.github.io/blob/master/python/code/COVID-tokyo.py の51，52行目を引用
+# ここまで
+# https://github.com/okumuralab/okumuralab.github.io/blob/master/python/code/COVID-tokyo.py の51，52行目を引用
 ax.bar(t[t >= t1], y[t >= t1], color=cols[t >= t1], width=DAY,
        align='edge', edgecolor="black", linewidth=0.5)
 ax.set_xlim(t1, t2)
-#ここまで
+# ここまで
 
 # グラフへ作成に日時を追加。dt_now_strを代入。(x,y)=(0.9,0.89)の右側へテキスト挿入
-fig.text(0.9, 0.89, 'generated: ' + dt_now_str, horizontalalignment='right')
-# グラフをいい感じにする。
-locator = mdates.AutoDateLocator()
-formatter = mdates.ConciseDateFormatter(locator)
-# 上の設定を代入
-ax.xaxis.set_major_locator(locator)
-ax.xaxis.set_major_formatter(formatter)
+fig.text(0.9, 0.89, '更新日時: ' + dt_now_str, horizontalalignment='right')
 # 凡例を追加。上部左
 ax.legend(['陽性者数'], loc='upper left')
 # グラフタイトルを追加。
-ax.set_title('鳥取県 コロナウイルス陽性者数の推移\n--'+maxt_date_print, loc='left', pad=30)
+ax.set_title('鳥取県 コロナウイルス陽性者数の推移\n___'+maxt_date_print, loc='left', pad=30)
 # 右側へ今日の感染者のラベルをつける。
 ax2 = ax.twinx()
 # 位置をｙ軸の最大値
 ax2.set_ylim(ax.get_ylim())
 ax2.set_yticks([y[-1]])
 path4 = os.path.join('Data', 'fig', 'graph', "tottori.svg")
-figpath2  = os.path.join(path1, path4)
+figpath2 = os.path.join(path1, path4)
 fig.savefig(figpath2, bbox_inches="tight")
